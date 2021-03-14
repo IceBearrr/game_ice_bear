5bbf9ad06c1f0b74fab1ae3c326369db6aa76ee6
IceBearrr <chirkarofl@gmail.com>
2021-03-14 13:30:06 +0300

    btn

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ document.addEventListener('keydown', function(event) {
     jump();
 });
 
+document.addEventListener('touchstart', function(event) {
+    jump();
+});
+
 function jump() {
     if (bear.classList != 'jump') {
         bear.classList.add('jump')
@@ -15,12 +19,21 @@ function jump() {
     }, 1500)
 }
 
+
+
+
 let isAlive = setInterval(function() {
     let bearTop = parseInt(window.getComputedStyle(bear).getPropertyValue('top'));
     let crabLeft = parseInt(window.getComputedStyle(crab).getPropertyValue('left'));
 
     if (crabLeft < 50 && crabLeft > 0 && bearTop <= 250 ) {
-        life.innerHTML = "<img src='pic/gameover.jpg' style='margin-left:25%'>"
-
+        document.body.style.backgroundImage = "url('pic/gameover.jpg')";
+        life.innerHTML = "<button class='resetbtn' aria-label='Начать заново' type='button' name='resetbtn'>Начать заново</button>"
+      
+        const btn = document.querySelector('.resetbtn');
+        document.addEventListener('click', function(event) {
+            location.reload();
+        });
     }
 }, 10)
+
